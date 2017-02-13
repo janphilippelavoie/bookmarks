13b44269ceb4ffb61d358b3d30d7d2d7febc1d9d
Jan-Philippe Lavoie <janphilippelavoie@gmail.com>
2017-02-12 22:53:48 -0500

    Added database configuration and liquibase plugin

--- src/main/java/com/jp/bookmarks/DropWizardTutorialConfiguration.java
+++ src/main/java/com/jp/bookmarks/DropWizardTutorialConfiguration.java
@@ -2,7 +2,10 @@ package com.jp.bookmarks;
 
 import io.dropwizard.Configuration;
 import com.fasterxml.jackson.annotation.JsonProperty;
+import io.dropwizard.db.DataSourceFactory;
 import org.hibernate.validator.constraints.*;
+
+import javax.validation.Valid;
 import javax.validation.constraints.*;
 
 public class DropWizardTutorialConfiguration extends Configuration {
@@ -10,8 +13,17 @@ public class DropWizardTutorialConfiguration extends Configuration {
     @NotEmpty
     private String password;
 
+    @NotNull
+    @Valid
+    private DataSourceFactory dataSourceFactory = new DataSourceFactory();
+
     @JsonProperty
     public String getPassword() {
         return password;
+    }
+
+    @JsonProperty("database")
+    public DataSourceFactory getDataSourceFactory() {
+        return dataSourceFactory;
     }
 }
